7b9308f124967e05a54d969edcd1c5954122d3f4
Felix Isaac <limfelix16@gmail.com>
2020-10-14 06:21:21 +0800

    Added item model

--- server/src/utils/encryption.ts
+++ server/src/utils/encryption.ts
@@ -1,2 +1,5 @@
-export const encryption = (text: string): string  => {}
-export const decryption = (encrypted: string): string => {};
+import Cryptr from "cryptr";
+const cryptr = new Cryptr(process.env.DB_ENCRYPTION_SECRET as string);
+
+export const encryption = cryptr.encrypt;
+export const decryption = cryptr.decrypt;
