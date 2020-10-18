7114c089b625b7815814b30a88475526db1c2665
Felix Isaac <limfelix16@gmail.com>
2020-10-18 22:31:53 +0800

    Add " item route schema"

---
+++ server/src/routes/item/item-route-schema.ts
@@ -0,0 +1,108 @@
+export const createCategorySchema = {
+    "schema": {
+        "body": {
+            "type": "object",
+            "required": ["name"],
+            "properties": {
+                "name": { "type":  "string" }
+            }
+        }
+    }
+}
+
+export const getCategorySchema = {
+    "schema": {
+        "params": {
+            "type": "object",
+            "properties": {
+                "categoryID": { "type": "string" }
+            }
+        }
+    }
+}
+
+export const editCategoryNameSchema = {
+    "schema": {
+        "params": {
+            "type": "object",
+            "properties": {
+                "categoryID": { "type": "string" }
+            }
+        },
+        "body": {
+            "type": "object",
+            "properties": {
+                "name": { "type": "string" }
+            }
+        }
+    }
+}
+
+export const deleteCategorySchema = {
+    "schema": {
+        "params": {
+            "types": "object",
+            "properties": {
+                "categoryID": { "type": "string" }
+            }
+        }
+    }
+};
+
+export const createItemSchema = {
+    "schema": {
+        "params": {
+            "type": "object",
+            "properties": {
+                "category": { "type": "string" }
+            }
+        },
+        "body": {
+            "type": "object",
+            "required": ["name", "image"],
+            "properties": {
+                "name": { "type": "string" },
+                "image": { "type": "string" }
+            }
+        }
+    }
+}
+
+export const editItemPropertiesSchema = {
+    "schema": {
+        "params": {
+            "type": "object",
+            "properties": {
+                "category": { "type": "string" },
+                "item": { "type": "string" },
+                "action": { "type": "string" }
+            }
+        },
+        "body": {
+            "type": "object",
+            "required": ["name", "image"],
+            "properties": {
+                "name": { "type": "string" },
+                "image": { "type": "string" }
+            }
+        }
+    }
+};
+
+export const updateItemStockSchema = {
+    "schema": {
+        "params": {
+            "type": "object",
+            "properties": {
+                "category": { "type": "string" },
+                "item": { "type": "string" }
+            }
+        },
+        "body": {
+            "type": "object",
+            "properties": {
+                "stock": { "type": "number" }
+            }
+        }
+    }
+}
