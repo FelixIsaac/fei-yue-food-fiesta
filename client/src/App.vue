8aa7ad7297f57fa5183a36b0da66fac414b6f36e
Felix Isaac <limfelix16@gmail.com>
2020-10-24 17:33:41 +0800

    Revert "Revert "Format files""
    This reverts commit 192b15698abffda2b8163ffa093b073747f3dd4a.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <Header/>
-    <router-view/>
-    <Footer/>
+    <Header />
+    <router-view />
+    <Footer />
   </div>
 </template>
 
 <script>
-    import Header from "@/components/Header";
-    import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-    export default {
-        components: {
-            Header,
-            Footer
-        }
-    };
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
 </script>
---
+++ client/src/components/Item.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  props: {
+    name: String,
+    image: String,
+    stock: Number,
+    category: String
+  }
+})
+export default class Counter extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
