4116af337d08c7e7c42b748c1dbf82b220ba2f22
Felix Isaac <limfelix16@gmail.com>
2020-10-25 22:02:02 +0800

    Add logo and items component

---
+++ client/src/components/Items.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="columns is-multiline">
+    <div class="column is-one-quarter" v-for="item in items" v-bind:key="item._id">
+      <Item
+        :item="item"
+        :category="category"
+        :show-admin-controls="showAdminControls"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+import Item from "./Item.vue";
+
+@Component({ props: { items: Array, category: {}, showAdminControls: Boolean }, components: { Item } })
+export default class Counter extends Vue {}
+</script>
